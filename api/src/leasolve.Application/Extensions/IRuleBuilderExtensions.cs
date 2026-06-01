d6e8c8f2d159d3b1e287318ae3bb75a0bbe9927b
Dominik Kowalczyk <kdominikpl@gmail.com>
2026-06-01 21:54:54 +0200

    feat(fluentValidation): add VO IRuleBuilderExtension
    Signed-off-by: Dominik Kowalczyk <kdominikpl@gmail.com>

---
+++ api/src/leasolve.Application/Extensions/IRuleBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using leasolve.Domain.Abstractions;
+
+namespace leasolve.Application.Extensions;
+
+public static class IRuleBuilderExtensions
+{
+    public static IRuleBuilder<T, TProperty> MustBeValidValueObject<T, TValue, TProperty>(
+        this IRuleBuilder<T, TProperty> ruleBuilder,
+        Func<TProperty, Result<TValue>> factory)
+    {
+        ArgumentNullException.ThrowIfNull(factory);
+
+        return ruleBuilder.Custom((value, context) =>
+        {
+            Result<TValue> result = factory(value);
+
+            if (!result.IsFailure)
+                return;
+            
+            context.AddFailure(new ValidationFailure(context.PropertyPath, result.Error.Details)
+            {
+                ErrorCode = result.Error.Code
+            });
+        });
+    }
+}
